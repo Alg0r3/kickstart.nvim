3fffdfd4bf93cd19f3da645d151f84184a78f6a1
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2025-02-01 18:51:41 +0100

    feat(dadbod): add db plugins with basic config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,25 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    vim.g.db_ui_win_position = 'right'
+    vim.g.db_ui_use_nerd_fonts = 1
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'dbui',
+      callback = function()
+        vim.opt_local.shiftwidth = 2
+        vim.opt_local.tabstop = 2
+      end,
+    })
+  end,
+}
